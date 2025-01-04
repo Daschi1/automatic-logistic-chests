3462ef2cf767e169c914a8820d92db5edfe3da34
Daschi1 <50054971+Daschi1@users.noreply.github.com>
2025-01-04 15:48:21 +0100

    refactor educt and product logic to live inside the specialized handle methods

--- runtime/recipe-handler.lua
+++ runtime/recipe-handler.lua
@@ -1,12 +1,57 @@
-local utils = require("runtime.utils")
+local inserter_handler                  = require("runtime.inserter-handler")
+local utils                             = require("runtime.utils")
 
-local recipe_handler = {}
+local recipe_handler                    = {}
 
 local recognized_standard_machine_types = {
     ["assembling-machine"] = true,
     ["furnace"]            = true,
     ["rocket-silo"]        = true,
 }
+
+--------------------------------------------------------------------------------
+--- Gather educt amounts based on inserters interacting with the chest.
+--- The function evaluates which inserters are picking items from the chest
+--- and retrieves the educt amounts from their drop targets.
+---
+--- @param chest LuaEntity # The chest entity being analyzed.
+--- @param surrounding_inserters LuaEntity[] # List of inserter entities surrounding the chest.
+--- @return table<string, number> # A table mapping item names to their respective amounts.
+--------------------------------------------------------------------------------
+function recipe_handler.gather_educts(chest, surrounding_inserters)
+    local educts_amounts = {}
+    for _, inserter in pairs(surrounding_inserters) do
+        if inserter_handler.is_inserter_picking_from_chest(inserter, chest) then
+            local drop_target = inserter.drop_target
+            if drop_target and drop_target.valid then
+                educts_amounts = recipe_handler.get_educts_amounts(drop_target)
+            end
+        end
+    end
+    return educts_amounts
+end
+
+--------------------------------------------------------------------------------
+--- Gather product amounts based on inserters interacting with the chest.
+--- The function evaluates which inserters are dropping items into the chest
+--- and retrieves the product amounts from their pickup targets.
+---
+--- @param chest LuaEntity # The chest entity being analyzed.
+--- @param surrounding_inserters LuaEntity[] # List of inserter entities surrounding the chest.
+--- @return table<string, number> # A table mapping item names to their respective amounts.
+--------------------------------------------------------------------------------
+function recipe_handler.gather_products(chest, surrounding_inserters)
+    local products_amounts = {}
+    for _, inserter in pairs(surrounding_inserters) do
+        if inserter_handler.is_inserter_dropping_into_chest(inserter, chest) then
+            local pickup_target = inserter.pickup_target
+            if pickup_target and pickup_target.valid then
+                products_amounts = recipe_handler.get_products_amounts(pickup_target)
+            end
+        end
+    end
+    return products_amounts
+end
 
 --------------------------------------------------------------------------------
 --- Gathers item requirements (educts) for a given entity (machine/turret/silo).
